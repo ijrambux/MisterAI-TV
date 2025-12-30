c73e7c847295bbb8c3600f52b46789e7cad11f7b
Mister AI <ijrambux@gmail.com>
2025-12-30 02:00:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# السيرفرات الخاصة بك
+# سيرفراتك التي تعمل
 XTREAM_SERVERS = [
     "http://fortv.cc:8080/get.php?username=1A63fh&password=337373&type=m3u",
     "http://tvhomesmart.xyz:8080/get.php?username=32930499&password=5req2f3q3&type=m3u_plus",
     "http://mytvstream.net:8080/get.php?username=TWEk66&password=036939&type=m3u_plus",
     "http://lobitv65.xyz:8080/get.php?username=svd2884&password=svd.475&type=m3u_plus",
-    "http://fruhd.cc:80/get.php?username=4428202673895240&password=4428202673895240&type=m3u_plus"
+    "http://fruhd.cc:80/get.php?username=4428202673895240&password=4428202673895240&type=m3u_plus",
+    "http://nuhygo.shop:8080/get.php?username=2643496sec&password=2643496sec&type=m3u_plus"
 ]
 
 @app.route('/')
